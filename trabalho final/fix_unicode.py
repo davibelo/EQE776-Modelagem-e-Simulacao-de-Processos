44d7bf86670629886f2d6c553d64b8b01772d7c7
Davi Belo <davibelo@gmail.com>
2025-11-07 14:29:34 -0300

    ok

--- trabalho final/fix_unicode.py
+++
@@ -1,9 +0,0 @@
-with open('trabalho.tex', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('\u2212', '-')
-
-with open('trabalho.tex', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print("Unicode characters replaced successfully!")
